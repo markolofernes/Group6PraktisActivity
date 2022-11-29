1a55cd0f51ba0c5fefdabc9ac5bec598522cfedc
markolofernes <markolofernes52615@gmail.com>
2022-11-30 00:58:26 +0800

    updated working hadicap and number of mines, must releas for fron-end design

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-var handycap = (document.getElementById("handiCap").value * 0.1);
+var handycap = 0.1;
 let points = 0;
-let clearTiles = 5;
+let pointsPerEvade = 150;
 function addPoints(){
-points += 1;
 
-
-
-if (points>=clearTiles){
+if (points>=(document.getElementById("numberMines").value)){
     clearalltiles();
-    document.getElementById('Message').innerHTML = clearTiles + ' Mines evaded! You Win!';
+    document.getElementById('Message').innerHTML = (document.getElementById("numberMines").value) + ' Mines evaded! You Win!';
     document.getElementById('Message').style = "color:blue";
-    // refreshPage();
 }
-    document.getElementById('points').innerHTML = ' Total Points: ' + points;
+    document.getElementById('points').innerHTML = ' Total Points: ' + points * pointsPerEvade;
 }
 
 function removetile(tile){
-    const test1 = Math.random() > handycap ? true : false;
-    if (test1==true) {
+    let mineBoolean = Math.random() > ((handycap) * (document.getElementById("handiCap").value)) ? true : false;
+    if (mineBoolean==true) {
         tile.parentElement.style.visibility = 'hidden';
+        points ++;
     }else{
-        clearalltiles();
         document.getElementById('Message').innerHTML = "G A M E   O V E R !";
         document.getElementById('Message').style = "color:red";
+        tile.parentElement.style = "background:red;border-radius:100px;";
+        tile.getElementsByClassName("btntile").className = "mineReveal";
+        clearalltiles();
     }
 }
 function refreshPage(){
@@ -33,16 +32,27 @@ function applyHandyCap(){
     var handycap = (document.getElementById("handiCap").value * 0.1);
 }
 
-function applynumberMines(){
+function changeMineno(){
     var clearTiles = (document.getElementById("numberMines").value);
     document.getElementById("minesToEvade").innerText = "Evade all " + clearTiles + " mines!";
 }
 function clearalltiles(){
     let tileNo = 0;                      
     while (tileNo < 16) {  
-    document.getElementById("btntiles"+tileNo).innerText = "X";
     document.getElementById("btntiles"+tileNo).className = 'disTiles';
+    document.getElementById("btntiles"+tileNo).innerText = "X";
     document.getElementById("btntiles"+tileNo).onclick = '';
+    document.getElementById("btntiles"+tileNo).style = "border-radius:100px";
     tileNo ++;    
     }
+    disableHandiCap();
+    applynumberMines();
+}
+function disableHandiCap() {
+    document.getElementById("handiCap").disabled = true;
+    document.getElementById("applyHndCap").disabled = true;
+}
+function applynumberMines() {
+    document.getElementById("numberMines").disabled = true;
+    document.getElementById("applyHndCap2").disabled = true;
 }
